83603790488e9b0cd02d22f95ec1c91b8fd804c8
chavinpat <chavinpatn@gmail.com>
2021-11-25 12:28:59 +0700

    add ar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARKit;
 
+[RequireComponent(typeof(ARFace))]
 public class PlayerMovement : CharacterMovement
-{
+{   
+    private ARFace face;
+    private ARKitFaceSubsystem faceSubsystem;
+
+    [SerializeField]
+    private Dictionary<ARKitBlendShapeLocation, float> cacheBlendShape;
+
+    private ARKitBlendShapeLocation blendShapeToTrack = ARKitBlendShapeLocation.JawOpen;
+
+    public float test_coef;
+
+    void Start() {
+    
+        face = GetComponent<ARFace>();
+        ARFaceManager faceManager = FindObjectOfType<ARFaceManager>();
+        
+        verticalDirection = 0;
+
+        if (faceManager != null) {
+            faceSubsystem = (ARKitFaceSubsystem)faceManager.subsystem;
+        }
+        face.updated += ArFace_Updated;
+
+
+    }
+
+    private void ArFace_Updated(ARFaceUpdatedEventArgs obj) {
+        cacheBlendShape = new Dictionary<ARKitBlendShapeLocation, float>();
+        using (var blendShapes = faceSubsystem.GetBlendShapeCoefficients(face.trackableId, Allocator.Temp)) {
+            foreach (var featureCoefficient in blendShapes) {
+                cacheBlendShape[featureCoefficient.blendShapeLocation] = featureCoefficient.coefficient;
+            }
+        test_coef = cacheBlendShape[blendShapeToTrack];
+            }
+    }
+
+    void OnDisable() {
+        face.updated -= ArFace_Updated;
+    }
+    
     private void Update() 
     {
-        verticalDirection = Input.GetAxis("Vertical");
+        
+        if (test_coef>0) {
+            verticalDirection = 1;
+        }
+        
         verticalDirection = Mathf.Clamp(verticalDirection, 0, 1);
 
         sprintValue = Input.GetAxis("Sprint");
